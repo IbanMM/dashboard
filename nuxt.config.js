673d9bafcc96c7fe5e3f5750915c10c67a1c5a82
iban <iban.moreno.martin@gmail.com>
2021-05-05 05:45:22 +0200

    scss vars colors, font, reset

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,9 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: '//fonts.googleapis.com', crossorigin: 'crossorigin' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap' }
     ]
   },
 
