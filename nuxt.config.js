408c4d7e9ef15d470e61e987f920206d652e1c11
iban <iban.moreno.martin@gmail.com>
2021-05-08 13:36:49 +0200

    babel warning is driven me crazy

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,5 +63,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    }
   }
 }
