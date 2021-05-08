d9beffd0c03ff011b868d3c9689388932370d562
iban <iban.moreno.martin@gmail.com>
2021-05-08 17:45:05 +0200

    index term list

--- pages/index.vue
+++ pages/index.vue
@@ -13,26 +13,35 @@
         />
       </div>
     </div>
-    <div class="row page-content">
-      <div class="col-lg-5">
-        <chart
-          :tit="'General Results'"
-          :amount="9041"
-          :amount-tit="'Followers'"
-          :bars="bars"
-        />
-      </div>
-      <div class="col-lg-5">
-        <chart
-          :tit="'Ratings by Category'"
-          :amount="3900"
-          :amount-tit="'Followers'"
-          :bars="bars"
-          :color="'#f0166d'"
-        />
-      </div>
-      <div class="col-lg-2">
-        chart
+    <div class="page-content">
+      <div class="row row-border-bottom">
+        <div class="col-lg-5 col-border-right">
+          <chart
+            :tit="'General Results'"
+            :amount="9041"
+            :amount-tit="'Followers'"
+            :bars="bars"
+          />
+        </div>
+        <div class="col-lg-5 col-border-right">
+          <chart
+            :tit="'Ratings by Category'"
+            :amount="3900"
+            :amount-tit="'Followers'"
+            :bars="bars"
+            :color="'#f0166d'"
+          />
+        </div>
+        <div v-if="terms.length" class="col-lg-2 flex-lg flex-col-lg justify-center-lg align-items-lg">
+          <ul class="terms_list">
+            <li v-for="(term,index) in terms" :key="index" class="terms_list_li flex-xs">
+              <span>
+                <span class="terms_item terms_item_tit">Term {{ index }}</span>
+                <span class="terms_item terms_item_amount">{{ term.amount }}</span>
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="col-lg-12">
         table
@@ -102,8 +111,42 @@ export default {
           size: 1,
           letter: 'n'
         }
+      ],
+      terms: [
+        {
+          amount: '85.00'
+        },
+        {
+          amount: '1.76'
+        },
+        {
+          amount: '33.42'
+        },
+        {
+          amount: '75.11'
+        }
       ]
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.terms_list_li {
+  padding: 0.4rem 0;
+}
+.terms_item {
+  text-transform: uppercase;
+  font-weight: 700;
+  display: inline-block;
+  padding: 0.2rem 0;
+  font-size: 0.8rem;
+}
+.terms_item_tit {
+  color: $grey-700;
+  padding-right: 1rem;
+}
+.terms_item_amount {
+  color: $dark;
+}
+</style>
